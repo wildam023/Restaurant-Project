a42bc7563b669e6e99b7577c18215b2843a57f2b
Williams <willemwilds@gmail.com>
2023-04-02 16:39:08 +0100

    Menu table modified

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -8,5 +8,5 @@ class Booking(models.Model):
 
 class Menu(models.Model):
     title = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField(default=5)
